e55c110526ff679bcd5fa6109c3e4e62d1c7d58e
Jane7510 <80323076+Jane7510@users.noreply.github.com>
2022-02-13 11:55:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function submit() {
+    document.getElementById("message").innerHTML = "Your form has been submitted successfully!!";
+
+    document.getElementById("name").value = "";
+    document.getElementById("last_name").value = "";
+
+    document.getElementById("male").checked = "";
+    document.getElementById("female").checked = "";
+
+    document.getElementById("C_no").value = "";
+    document.getElementById("address").value = "";
+
+    document.getElementById("hobby1").checked = "";
+    document.getElementById("hobby2").checked = "";
+    document.getElementById("hobby3").checked = "";
+    document.getElementById("hobby4").checked = "";
+    document.getElementById("hobby5").checked = "";
+    document.getElementById("hobby6").checked = "";
+}
